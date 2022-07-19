c4e68c69813d9f617da9162b1f2bce4027c3b037
Maho13 <109622635+Maho13@users.noreply.github.com>
2022-07-19 13:25:57 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Sabía que querías, me encantas mucho anshbshvbgsnjncjv')
+    alert('MI BLANQUITO PRECIOSOO')
+});
+
+const noBtn = document.querySelector('noBtn');
+
+noBtn.addEventListener('Mouseover', () => {
+        randomX = parseInt(Math.random() * 100);
+        randomY = parseInt(Math.random() * 100);
+
+        noBtn.style.setProperty('top', randomX + '%');
+        noBtn.style.setProperty('left', randomY + '%');
+
+        noBtn.style.setProperty('transform', `translate(-${randomX}%,-${randomY}%)`);
+    })
